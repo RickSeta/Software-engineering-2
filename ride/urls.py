d81ca6aee21c0c4b97ee86282f08414de369575a
MarcozFe <marcoz.araujoz@gmail.com>
2024-04-24 22:15:00 -0300

    pagina de login

--- ride/urls.py
+++ ride/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from ride.views.create_ride_view import CreateRideView
 from ride.views.profile_view import ProfileView
 from ride.views.search_ride_view import SearchRideView
+from ride.views.login_view import LoginView
 
 
 urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
     path('criar/', CreateRideView.as_view(), name='create_ride'),
     path('perfil/', ProfileView.as_view(), name='profile'),
     path('buscar/', SearchRideView.as_view(), name='search_ride'),
