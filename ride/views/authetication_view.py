cf71dac90cb940341931beba07ac4e8e0290ba14
MarcozFe <marcoz.araujoz@gmail.com>
2024-06-04 17:23:03 -0300

    Cadastro feito completo

--- ride/views/authetication_view.py
+++ ride/views/authetication_view.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from ride.forms.auth_form import SignupForm
+from ride.models import UserProfile
 
 
 def authView(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST or None)
+        form = SignupForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            profile = UserProfile(user=user)
+            profile.save()
+            messages.success(request, "Criou conta com sucesso!")
             return redirect("ride:login")
     else:
-        form = UserCreationForm()
+        form = SignupForm()
     return render(request, "registration/signup.html", {"form" : form})
