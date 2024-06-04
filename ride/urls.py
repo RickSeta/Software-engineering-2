477fee98231695a929c9a3dc1fcb6a9820aa8d1f
RickSeta <54965077+RickSeta@users.noreply.github.com>
2024-06-03 21:08:09 -0300

    feat: editing implemented

--- ride/urls.py
+++ ride/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
+from django.conf import settings
 from ride.views.create_ride_view import CreateRideView
 from ride.views.profile_view import ProfileView
 from ride.views.search_ride_view import SearchRideView
-
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('criar/', CreateRideView.as_view(), name='create_ride'),
     path('perfil/', ProfileView.as_view(), name='profile'),
     path('buscar/', SearchRideView.as_view(), name='search_ride'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
