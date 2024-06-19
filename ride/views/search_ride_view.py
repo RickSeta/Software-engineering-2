a221d0a86fad93b4de122c693629ea198bb130d4
Thiago Laet <thi4golaet@gmail.com>
2024-06-19 18:52:04 -0300

    add maps to the search ride screen

--- ride/views/search_ride_view.py
+++ ride/views/search_ride_view.py
@@ -26,58 +26,31 @@ class SearchRideView(LoginRequiredMixin, GoogleMapsAPIMixin, TemplateView):
     def post(self, request):
         try:
             data = json.loads(request.body)
-            starting_point_name = data.get('starting_point')
-            destination_name = data.get('destination')
-            starting_point_coords = data.get('starting_point_coords').split(', ')
-            destination_coords = data.get('destination_coords').split(', ')
             starting_hour = data.get('starting_hour')
             passengers = data.get('passengers')
+            import ipdb; ipdb.set_trace()
+            starting_point_location, _ = Location.objects.get_or_create(
+                latitude=data['starting_point_latitude'], longitude=data['starting_point_longitude'],
+                defaults={'address': data['starting_point_address']}
+            )
 
-            #bugfix pois estava achando multiplos results
-            starting_point_location = Location.objects.filter(
-                latitude=starting_point_coords[0], longitude=starting_point_coords[1]
-            ).first()
-            if not starting_point_location:
-                starting_point_location = Location.objects.create(
-                    latitude=starting_point_coords[0], longitude=starting_point_coords[1]
-                )
-
-            destination_location = Location.objects.filter(
-                latitude=destination_coords[0], longitude=destination_coords[1]
-            ).first()
-            if not destination_location:
-                destination_location = Location.objects.create(
-                    latitude=destination_coords[0], longitude=destination_coords[1]
-                )
-
-            #bugfix pois estava consultando por loc ao inves de coord
-            starting_point = Place.objects.filter(
-                location=starting_point_location
-            ).first()
-            if not starting_point:
-                starting_point = Place.objects.create(
-                    name=starting_point_name, location=starting_point_location
-                )
-
-            destination = Place.objects.filter(
-                location=destination_location
-            ).first()
-            if not destination:
-                destination = Place.objects.create(
-                    name=destination_name, location=destination_location
-                )
+            destination_location, _ = Location.objects.get_or_create(
+                latitude=data['destination_latitude'], longitude=data['destination_longitude'],
+                defaults={'address': data['destination_address']}
+            )
 
             starting_hour_naive = timezone.datetime.strptime(starting_hour, '%Y-%m-%dT%H:%M')
             starting_hour_aware = timezone.make_aware(starting_hour_naive, timezone.get_default_timezone())
 
             #filtra caronas disponíveis
             rides = Ride.objects.filter(
-                starting_point=starting_point.location,
-                destination=destination.location,
-                starting_hour__date=starting_hour_aware.date(),  
-                starting_hour__hour=starting_hour_aware.hour,    
-                available_seats__gte=passengers
-            )
+                starting_point=starting_point_location,
+                destination=destination_location,
+                starting_hour__date=starting_hour_aware.date(),
+                starting_hour__hour=starting_hour_aware.hour,
+                available_seats__gte=passengers,
+                status=RideStatus.SCHEDULED.value
+            ).exclude(car__owner=request.user.userprofile)
 
             if rides.exists():
                 rides_data = [{
@@ -89,22 +62,22 @@ class SearchRideView(LoginRequiredMixin, GoogleMapsAPIMixin, TemplateView):
                     'owner_rating': ride.car.owner.rating,
                     'starting_hour': ride.starting_hour,
                     'available_seats': ride.available_seats,
-                    'starting_point': starting_point.name,
-                    'destination': destination.name
+                    'starting_point': starting_point_location.address,
+                    'destination': destination_location.address
                 } for ride in rides]
                 return JsonResponse({'rides': rides_data})
             else:
                 user_profile = UserProfile.objects.get(user=request.user)
                 ride_request= RideRequest.objects.create(
-                    starting_point=starting_point.location,
-                    destination=destination.location,
+                    starting_point=starting_point_location,
+                    destination=destination_location,
                     starting_hour=starting_hour_aware,
                     created_by=user_profile
                 )
                 ride_request.save()
                 resp = {
-                    'starting_point': starting_point.name,
-                    'destination': destination.name,
+                    'starting_point': starting_point_location.address,
+                    'destination': destination_location.address,
                     'starting_hour': starting_hour_aware,
                     'created_by': user_profile.user.username
                 }
